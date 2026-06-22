2107f211bc1487b15506bc9b58e62b6aec357ab6
MuHucTep <alexandersyrovatskij@gmail.com>
2026-06-22 15:13:04 +0600

    upgrade db models catalog

--- catalog/models.py
+++ catalog/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class GiftModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category_id = models.IntegerField()
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=False, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    category_id = models.IntegerField(null=False, blank=False)
     available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
     
 class CategoryModel(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(null=False, blank=True)
 
     def __str__(self):
         return self.name
