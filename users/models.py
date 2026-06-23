3cb8b30b3678b56829a8da3e5e19c48a33875118
MuHucTep <alexandersyrovatskij@gmail.com>
2026-06-23 14:58:23 +0600

    add user and cart models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class UserModel(models.Model):
+    name = models.CharField(max_length=150)
+    second_name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    cart_id = models.IntegerField(null=False, blank=True)
+
+    def __str__(self):
+        return f'{self.id} {self.name} {self.second_name}'
